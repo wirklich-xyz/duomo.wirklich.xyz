0bbad2dc8465fa50f10977594f97d98607581890
Ralf Ulrich <ralf.ulrich@braintribe.com>
2023-10-11 10:00:07 +0200

    actions

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	}
+};
+
+config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
+
+export default config;
