12ebc660c16154ed3f5f454b7b3f054603d63de5
Ralf Ulrich <ralf.ulrich@braintribe.com>
2023-10-13 10:16:35 +0200

    impressum

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,19 +7,6 @@
   let infoLines;
 </script>
 
-<svelte:head>
-  <title>Duomo Milano Sun-Calendar</title>
-</svelte:head>
-
-<header>
-  <select bind:value={$locale}>
-    {#each locales as l}
-      <option value={l}>{l}</option>
-    {/each}
-  </select>
-  <h1>{$t("homepage.title")}</h1>
-</header>
-
 <main>
   <InfoLines bind:infoLines locale={$locale} />
   <br />
@@ -27,24 +14,12 @@
 </main>
 
 <style>
-  header {
-    text-align: center;
-    width: 100%;
-    margin: 0 auto;
-    background-color: rgb(174, 153, 153);
-  }
   :global(main) {
     width: 700px;
     text-align: center;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #4a1c0d;
-    font-size: 3.5em;
-    font-weight: 100;
   }
 
   @media (min-width: 640px) {
@@ -64,14 +39,5 @@
     margin: 4px 2px;
     cursor: pointer;
   }
-  select {
-    border: 1px solid #411212;
-    background-color: rgba(255, 255, 255, 0.2);
-    padding: 1px;
-    float: right;
-    margin: 3px;
-    height: auto;
-    font-size: x-small;
-    line-height: 10px;
-  }
+
 </style>
