21c99f81ca7d42a6d61e68501b6e193313ca4d1c
Ralf Ulrich <ralf.ulrich@braintribe.com>
2023-10-16 14:36:47 +0200

    flags and i18n

---
+++ src/lib/translations.ts
@@ -0,0 +1,125 @@
+import { de, enGB, it } from "date-fns/locale/index.js";
+
+export default {
+  en: {  
+    "fns": enGB,
+    "icon": "Gb",
+
+    "homepage.title": "Duomo Milano Sun-Calendar",
+
+    "homepage.welcome": "Date of visit:",
+
+    "homepage.footer": `Note, you need to book a ticket to visit the Duomo. There are different options,
+      the typical trip includes the "terraces", which basically means you enter the building
+      on the north-side and directly access the rooftop of the building. You will enter
+      the interior of the building going down from the roof. Consider at least 30-45min
+      for the terraces.
+      <br />
+      <a
+        class="button"
+        href="https://ticket.duomomilano.it/en/biglietti/"
+        target="_blank">Go to official booking site (link)</a
+      ><br />
+      Better double-check weather and cloud predictions:
+      <a
+        href="https://www.accuweather.com/de/it/milan/214046/daily-weather-forecast/214046"
+        target="_blank">AccuWeather</a
+      >
+      or
+      <a href="https://www.bergfex.it/sommer/mailand/wetter/" target="_blank"
+        >bergfex.it</a
+      >`,
+
+    "data.day": "Day",
+
+    "data.date": "Date",
+
+    "data.time": "Time",
+
+    "data.weather": "Clouds",
+
+    "text.weather": "Weather",
+  },
+
+
+  de: {
+    "fns": de,
+    "icon": "De",
+
+    "homepage.title": "Duomo Milano Sonnenkalender",
+
+    "homepage.welcome": "Datum des Besuchs:",
+
+    "homepage.footer": `Achtung, zum Besuch des Doms wird ein Ticket benötigt. Das typische Ticket 
+    beinhaltet einen Besuch der "terrazze". Das bedeutet man betritt den Dom an der Nordseite
+    und geht dort direkt auf das Dach des Doms. In das Innere des Doms kommt man dann auf dem Abstieg vom Dach.  
+    Planen sie mindestens 30-45min für die "terrazze" ein (plus Wartezeit am Eingang). 
+      <br />
+      <a
+        class="button"
+        href="https://ticket.duomomilano.it/en/biglietti/"
+        target="_blank">Link zur offiziellen Buchungsseite</a
+      ><br />
+      Bitte die Wetterbedingungen noch einmal kontrollieren: 
+      <a
+        href="https://www.accuweather.com/de/it/milan/214046/daily-weather-forecast/214046"
+        target="_blank">AccuWeather</a
+      >
+      oder
+      <a href="https://www.bergfex.it/sommer/mailand/wetter/" target="_blank"
+        >bergfex.it</a
+      >`,
+
+    "data.day": "Tag",
+
+    "data.date": "Datum",
+
+    "data.time": "Zeitpunkt",
+
+    "data.weather": "Wolken",
+
+    "text.weather": "Wetter"
+  },
+
+  it: {
+    "fns": it,
+    "icon": "It",
+
+    "homepage.title": "Calendario Solare del Duomo Milano",
+
+    "homepage.welcome": "Data della visita",
+
+    "homepage.footer": `Nota: È necessario prenotare un biglietto per visitare il Duomo. Ci sono diverse opzioni,
+    il tour tipico include le "terrazze", il che significa essenzialmente che si entra nell'edificio
+    dal lato nord e si accede direttamente al tetto dell'edificio. Successivamente, si accede
+    all'interno dell'edificio scendendo dal tetto. Prevedi almeno 30-45 minuti
+    per visitare le terrazze.
+    <br />
+    <a
+      class="button"
+      href="https://ticket.duomomilano.it/en/biglietti/"
+      target="_blank">Vai al sito ufficiale per la prenotazione (link)</a
+    ><br />
+    È meglio verificare le previsioni meteorologiche e delle nuvole:
+    <a
+      href="https://www.accuweather.com/de/it/milan/214046/daily-weather-forecast/214046"
+      target="_blank">AccuWeather</a
+    >
+    o
+    <a href="https://www.bergfex.it/sommer/mailand/wetter/" target="_blank"
+      >bergfex.it</a
+    >`,
+
+    "data.day": "Giorno",
+
+    "data.date": "Data",
+
+    "data.time": "Orario",
+
+    "data.weather": "Nuvole",
+
+    "text.weather": "Tempo",
+  },
+
+};
+
